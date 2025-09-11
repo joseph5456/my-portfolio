2d8f8c0a4a9d6ac0409453418668c8c7ee8a12ee
joseph <tracy201156@gmail.com>
2025-09-11 18:45:17 -0400

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,90 @@
 import Image from "next/image";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaG } from "react-icons/fa6";
+import headshot from"../public/headshot.png"
+
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen bg-gray-800 text-white flex items-center justify-start flex-col">
+      <nav className="flex justify-between py-8 w-full max-w-4xl ">
+        <h1 className="font-bold text-lg">Joseph Nguyen</h1>
+        <a className="bg-sky-700 px-4 py-1 rounded-sm border-b-2 border-sky-800 ring ring-sky-800 hover:bg-white hover:text-sky-700 transition-colors focus:border-b-0 focus:mt-0.5" href="/resume.pdf">Resume</a>
+      </nav>  
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <header className="w-full max-w-prose space-y-8">
+      <figure className="size-48 rounded-full overflow-hidden">
+        <Image src={headshot} alt="Joseph Nguyen" />
+      </figure>
+
+        
+        <h1 className="font-bold text-4xl">Hi, I'm Joseph!</h1>
+
+        <p className="text-lg">Designing custom websites, building etc placeholder</p>
+
+        <ul className="flex text-3xl gap-4">
+          <li><a href="https://github.com/joseph5456"><FaGithub /></a></li>
+          <li><a href="https://www.linkedin.com/in/joseph-nguyen-685711334/"><FaLinkedin /></a></li>
+        </ul>
+      </header> 
+
+      <section className="w full max-w-prose space-y-8">
+        <h2 className="text-2xl text-sky-400 font-bold">Education</h2>
+
+        <div className="flex">
+          <article className="flex-1">
+            <p><b>University of Georgia </b></p>
+            <p><i>B.S. in Computer Science GPA: 3.78</i></p>
+          </article>
+          <article className="text-right">
+            <p>Expected Gradution date: May 2028</p>
+          </article>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+
+      <section className="w full max-w-prose space-y-8">
+        <h2 className="text-2xl text-sky-400 font-bold">Work Experience</h2>
+
+          <article>
+            <h3>
+              <span><b>Customer Service Representative</b></span>
+              <span>May 2023 - Dec 2023</span>
+            </h3>
+            <ul className="list-disc pl-6 text-sm">
+              <li> Managed client communications and appointment coordination for dental laboratory services, ensuring seamless scheduling
+              and customer satisfaction
+              </li>
+            </ul>
+            <p><b>University of Georgia Athens, GA </b></p>
+            <p><i>B.S. in Computer Science GPA: 3.78</i></p>
+          </article>
+          <article className="text-right">
+            <p>Expected Gradution date: May 2028</p>
+          </article>
+      </section>
+
+      <section className="w full max-w-prose space-y-4">
+        <h2 className="text-2xl text-sky-400 font-bold">Projects</h2>
+
+        <div className="grid-cols-2">
+          <a href="https://github.com/joseph5456/world-eco-data" className="bg-sky-700 gap-2 shadow-md transition:shadow hover:shadow">
+            <span>something</span>
+            <span>something</span>
+          </a>
+          </div>
+        </section>
+
+
+        <footer className="flex justify-between py-8 w-full max-w-4xl border-t border-gray-500 ">
+          <p className="text-gray-400">
+            &copy; 2025 Joseph Nguyen
+          </p>
+          <ul className="flex text-xl gap-4">
+          <li><a href="https://github.com/joseph5456"><FaGithub /></a></li>
+          <li><a href="https://www.linkedin.com/in/joseph-nguyen-685711334/"><FaLinkedin /></a></li>
+        </ul>
+        </footer>
+        </div>
+  
   );
 }
